72274fe23e1108f4f33a62f394fb44a1fe4b0746
manishpun08 <manishpunmagar6@gmail.com>
2025-02-14 21:13:21 +0545

    Implement Search Functionality

--- components/search/Searchbar.tsx
+++ components/search/Searchbar.tsx
@@ -1,17 +1,51 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import { FormControl, InputAdornment, OutlinedInput } from '@mui/material';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { formUrlQuery, removeKeysFormQuery } from '@/lib/url';
 
-const SearchBar = () => {
-  const [search, setSearch] = useState('');
+interface Props {
+  route: string;
+}
 
+const SearchBar = ({ route }: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const searchParams = useSearchParams();
+  const query = searchParams.get('query') || '';
+
+  const [searchQuery, setSearchQuery] = useState(query);
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (searchQuery) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: 'query',
+          value: searchQuery,
+        });
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === route) {
+          const newUrl = removeKeysFormQuery({
+            params: searchParams.toString(),
+            keysToRemove: ['query'],
+          });
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 500);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchQuery, router, route, searchParams, pathname]);
   return (
     <div>
       <FormControl fullWidth variant="outlined">
         <OutlinedInput
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
           startAdornment={
             <InputAdornment position="start">
               <SearchIcon />
